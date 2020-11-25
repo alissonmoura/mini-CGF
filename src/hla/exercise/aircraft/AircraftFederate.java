3fdfedaf7cd8ae6d8667240efaace69f566d0796
Igor Genuino <igor.margen@gmail.com>
2020-11-25 16:04:57 +0100

    connecting and creating Aircraft Federate

--- src/hla/exercise/aircraft/AircraftFederate.java
+++ src/hla/exercise/aircraft/AircraftFederate.java
@@ -2,16 +2,59 @@ package hla.exercise.aircraft;
 
 import hla.aircraft.AircraftCallback;
 import hla.aircraft.HlaAircraft;
-import hla.rti1516e.exceptions.RTIexception;
+import hla.rti1516e.*;
+import hla.rti1516e.encoding.EncoderFactory;
+import hla.rti1516e.exceptions.*;
 
-public class AircraftFederate {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+public class AircraftFederate extends NullFederateAmbassador {
+    private RtiFactory rtiFactory;
+    private RTIambassador rtIambassador;
+    private URL formModule;
+    private String formModuleName;
+    private ObjectClassHandle objectClassHandle;
+    private String federationType;
+    private String federationExecutionName;
+    private EncoderFactory encoderFactory;
+
 
     public AircraftFederate(AircraftCallback aircraftCallback) {
-        // TODO Auto-generated constructor stub
+        formModuleName = "C:\\Users\\User1\\IdeaProjects\\CFG\\mini-CGF\\HLA-course.xml";
+        federationExecutionName = "AircraftDestination";
+        federationType = "Aircraft";
     }
 
-    public void init() {
-        // TODO Auto-generated method stub
+    public AircraftFederate() {
+
+
+    }
+
+    public void init() throws RTIinternalError, AlreadyConnected, CallNotAllowedFromWithinCallback, UnsupportedCallbackModel, ConnectionFailed, InvalidLocalSettingsDesignator, MalformedURLException, SaveInProgress, RestoreInProgress, FederateAlreadyExecutionMember, FederationExecutionDoesNotExist, CouldNotCreateLogicalTimeFactory, NotConnected, NameNotFound, FederateNotExecutionMember {
+        rtiFactory = RtiFactoryFactory.getRtiFactory();
+        encoderFactory  = rtiFactory.getEncoderFactory();
+        rtIambassador = rtiFactory.getRtiAmbassador();
+        rtIambassador.connect(this, CallbackModel.HLA_IMMEDIATE);
+        formModule = Paths.get(formModuleName).toUri().toURL();
+        try {
+            rtIambassador.createFederationExecution(federationExecutionName,formModule);
+        } catch (FederationExecutionAlreadyExists e) {
+            System.out.println("Failed");
+        } catch (NotConnected notConnected) {
+            notConnected.printStackTrace();
+        } catch (CouldNotOpenFDD couldNotOpenFDD) {
+            couldNotOpenFDD.printStackTrace();
+        } catch (ErrorReadingFDD errorReadingFDD) {
+            errorReadingFDD.printStackTrace();
+        } catch (InconsistentFDD inconsistentFDD) {
+            inconsistentFDD.printStackTrace();
+        }
+
+        rtIambassador.joinFederationExecution(federationType,federationExecutionName);
+        objectClassHandle = rtIambassador.getObjectClassHandle("HLAobjectRoot.Aircraft");
+
 
     }
 
@@ -21,7 +64,7 @@ public class AircraftFederate {
     }
 
     public void update(HlaAircraft aircraft) throws RTIexception {
-        // TODO Auto-generated method stub
+
 
     }
 }
